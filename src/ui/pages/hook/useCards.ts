1ca58ff5d92f705dc99774c1104fd081fb212b68
fidh3 <giorgio.fidelio@edu.itspiemonte.it>
2025-01-23 10:44:43 +0100

    feat ho aggiusto useCards, dove ho iniziato a mettere le basi per lo storage, ma attualmente l'ho usato solo per impotare un'interface comune

--- src/ui/pages/hook/useCards.ts
+++ src/ui/pages/hook/useCards.ts
@@ -2,15 +2,18 @@ import { useState, useCallback } from 'react';
 import { PREFERRED_CARDS } from '../../../core/storage/types';
 import { storage } from '../../../core/storage/storage';
 
-interface ProductCard {
 
-    id: number;
-    title: string;
-    price: number;
-    category: string;
-    image: string
-    rate: number;
-}
+export interface ProductCard {
+
+  id: number;
+  title: string;
+  price: number;
+  description: string,
+  category: string;
+  image: string
+  rate: number;
+  count: number;
+} 
 
 
 
@@ -19,7 +22,17 @@ export const useCards = () => {
   const [initialCards, setInitialCards] = useState<ProductCard[]>([]);
   const [favoriteIds, setFavoriteIds] = useState<number[]>([]);
 
-  
+  const refreshCards = useCallback(async () => {
+    try {
+      const response = await fetch('https://fakestoreapi.com/products');
+      const data = await response.json();
+      setInitialCards([...data.cards]);
+      setCards([...data.cards]);
+    } catch (error) {
+      console.error('Error fetching carts:', error);
+    }
+  }, []);
+
   const loadFavorites = useCallback(async () => {
     try {
       const storedFavorites = await storage.getItem(PREFERRED_CARDS);
